7537e7275043fbde91298384d164598382ae6a2f
Rocco Palermiti <RokyMac@Roccos-MacBook-Pro.local>
2018-01-08 19:59:59 +0100

    now includes some of allegro libraries

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+
+int main(int argc, char **argv){
+
+
+
+  return 0;
+}
