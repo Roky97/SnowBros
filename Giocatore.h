f266391f41445255b712984998b1d9a838ed58e9
Rocco Palermiti <RokyMac@Roccos-MacBook-Pro.local>
2018-02-21 11:24:42 +0100

    change int to unsigned and added some functions

--- Giocatore.h
+++ Giocatore.h
@@ -1,17 +1,26 @@
 #ifndef GIOCATORE_H
 #define GIOCATORE_H
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
 class Giocatore{
 private:
-  int x;
-  int y;
-  int velocita;
-  int vite;
+  unsigned x;
+  unsigned y;
+  unsigned velocita;
+  unsigned vite;
 
   bool andando_destra;
   bool andando_sinistra;
   bool saltando;
   bool sparando;
+  bool potere_v;
 
 
 public:
@@ -21,150 +30,42 @@ public:
 	//sinistra
 	ALLEGRO_BITMAP *verso_sinistra1;
 	ALLEGRO_BITMAP *verso_sinistra2;
-	ALLEGRO_BITMAP *verso_sinistra3;
+	//ALLEGRO_BITMAP *verso_sinistra3;
 
 	//destra
 	ALLEGRO_BITMAP *verso_destra1;
 	ALLEGRO_BITMAP *verso_destra2;
-	ALLEGRO_BITMAP *verso_destra3;
+	//ALLEGRO_BITMAP *verso_destra3;
 
   //spara
   ALLEGRO_BITMAP *sparando_verso_destra;
-  ALLEGRO_BITMAP *spaando_verso_sinistra;
+  ALLEGRO_BITMAP *sparando_verso_sinistra;
 
-  Player();
+  Giocatore();
   //sets
-  void setX(int sx);
-  void setY(int sy);
-  void setVelocita(int v);
-  void setVite(int v);
+  void setX(unsigned sx);
+  void setY(unsigned sy);
+  void setVelocita(unsigned v);
+  void setVite(unsigned v);
   void setAndando_destra(bool d);
   void setAndando_sinistra(bool s);
   void setSaltando(bool s);
   void setSparando(bool s);
   //get
-  void getX();
-  void getY();
-  void getVelocita();
-  void getVite();
-  void getAndando_destra();
-  void getAndando_sinistra();
-  void getSaltando();
-  void getSparando();
+  unsigned getX();
+  unsigned getY();
+  unsigned & getVelocita();
+  unsigned & getVite();
+  bool getAndando_destra();
+  bool getAndando_sinistra();
+  bool getSaltando();
+  bool getSparando();
 
   void carica_immagini();
+  void diminiusci_vita();
+  void potere_verde();
 
 
 
 };
-
-Player()
-{
-  x=170;
-  y=128;
-  speed=1;
-  vite=3;
-
-  salta= NULL;
-
-	verso_sinistra1= NULL;
-	verso_sinistra2= NULL;
-	verso_sinistra3= NULL;
-
-	verso_destra1= NULL;
-	verso_destra2= NULL;
-	verso_destra3= NULL;
-
-  sparando_verso_destra= NULL;
-  sparando_verso_sinistra= NULL;
-
-}
-
-void setX(int sx)
-{
-  x=sx;
-}
-
-void setY(int sy)
-{
-  y=sy;
-}
-
-void setVelocita(int v)
-{
-  velocita=v;
-}
-void setVite(int v)
-{
-  vite=v;
-}
-
-void setAndando_destra(bool d)
-{
-  andando_destra=d;
-}
-void setAndando_sinistra(bool s)
-{
-  andando_sinistra=s;
-}
-void setSaltando(bool s)
-{
-  saltando=s;
-}
-void setSparando(bool s)
-{
-  sparando=s;
-}
-
-int getX()
-{
-  return x;
-}
-int getY()
-{
-  return y;
-}
-int getVelocita()
-{
-  return velocita;
-}
-int getVite()
-{
-  return vite;
-}
-bool getAndando_destra()
-{
-  return andando_destra;
-}
-bool getAndando_sinistra()
-{
-  return andando_sinistra;
-}
-bool getSaltando()
-{
-  return saltando;
-}
-bool getSparando()
-{
-  return sparando;
-}
-
-void carica_immagini()
-{
-  salta= al_load_bitmap("./images/giocatore/salto.png");
-
-	verso_sinistra1= al_load_bitmap("./images/giocatore/sinistra_1.png");
-	verso_sinistra2= al_load_bitmap("./images/giocatore/sinistra_2.png");
-	//verso_sinistra3= al_load_bitmap("./images/giocatore/");
-
-	verso_destra1= al_load_bitmap("./images/giocatore/destra_1.png");
-	verso_destra2= al_load_bitmap("./images/giocatore/destra_2.png");
-	//verso_destra3= al_load_bitmap("./images/giocatore/");
-
-  sparando_verso_destra=al_load_bitmap("./images/giocatore/sparo_dx1.png");
-  sparando_verso_sinistra=al_load_bitmap("./images/giocatore/sparo_sx1.png");
-}
-
-
-
 #endif
