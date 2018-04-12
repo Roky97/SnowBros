2bf79676516bdc079e625118dbdf6dc01df928e4
Micurucceja <dome.ingrati@gmail.com>
2018-04-12 21:17:47 +0200

    Added Pumpkin

---
+++ Zucca.cpp
@@ -0,0 +1,137 @@
+#include "Zucca.h"
+
+Zucca::Zucca()
+{
+  x=0;
+  y=0;
+  cont=0;
+  changeBitmap=false;
+  spostamento=7;
+  vita=false;
+  andando_destra=false;
+  andando_sinistra=false;
+  dx1=al_load_bitmap("./images/zucca/destra1.png");
+  dx2=al_load_bitmap("./images/zucca/destra2.png");
+
+  sx1=al_load_bitmap("./images/zucca/sinistra1.png");
+  sx2=al_load_bitmap("./images/zucca/sinistra2.png");
+}
+
+Zucca::Zucca(float posx, float posy)
+{
+  x=posx;
+  y=posy;
+  cont=0;
+  changeBitmap=false;
+  spostamento=3;
+  vita=false;
+  andando_destra=false;
+  andando_sinistra=false;
+  dx1=al_load_bitmap("./images/zucca/destra1.png");
+  dx2=al_load_bitmap("./images/zucca/destra2.png");
+
+  sx1=al_load_bitmap("./images/zucca/sinistra1.png");
+  sx2=al_load_bitmap("./images/zucca/sinistra2.png");
+}
+
+Zucca::~Zucca()
+{
+  al_destroy_bitmap(dx1);
+  al_destroy_bitmap(dx2);
+  al_destroy_bitmap(sx1);
+  al_destroy_bitmap(sx2);
+}
+
+
+void Zucca::setVita(bool v)
+{
+  vita=v;
+}
+
+bool Zucca::getVita()
+{
+  return vita;
+}
+
+void Zucca::muoviZucca(float posX, float posY) //PASSIAMO LE POSIZIONI DEL PERSONAGGIO IN MODO DA RAGGIUNGERLO OVUNQUE ESSO SIA
+{
+    if(posX>=x)
+    {
+      x+=spostamento;
+      andando_destra=true;
+      andando_sinistra=false;
+    }
+
+    if(posY>=y)
+    {
+      y+=spostamento;
+      andando_destra=true;
+      andando_sinistra=false;
+    }
+
+    if(posX<x)
+    {
+      x-=spostamento;
+     andando_sinistra=true;
+     andando_destra=false;
+    }
+
+    if(posY<y)
+    {
+      y-=spostamento;
+      andando_sinistra=true;
+      andando_destra=false;
+    }
+
+}
+void Zucca::drawZucca()
+{
+  if(andando_destra && !changeBitmap)
+  {
+      al_draw_scaled_bitmap(dx1, 0, 0, 30, 30, x, y, 30*6, 30*6, 0);
+      cont++;
+
+       if(cont==7)
+       {
+       changeBitmap=true;
+       cont=0;
+       }
+  }
+
+
+  else if(andando_destra && changeBitmap)
+  {
+      al_draw_scaled_bitmap(dx2, 0, 0, 30, 30, x, y, 30*6, 30*6, 0);
+      cont++;
+
+       if(cont==7)
+       {
+        changeBitmap=false;
+        cont=0;
+       }
+  }
+
+  else if(andando_sinistra && !changeBitmap)
+  {
+    al_draw_scaled_bitmap(sx1, 0, 0, 30, 30, x, y, 30*6, 30*6, 0);
+    cont++;
+
+     if(cont==7)
+     {
+     changeBitmap=true;
+     cont=0;
+     }
+  }
+
+  else if(andando_sinistra && changeBitmap)
+  {
+    al_draw_scaled_bitmap(sx2, 0, 0, 30, 30, x, y, 30*6, 30*6, 0);
+    cont++;
+
+     if(cont==7)
+     {
+     changeBitmap=false;
+     cont=0;
+     }
+  }
+}
---
+++ Zucca.h
@@ -0,0 +1,48 @@
+#ifndef ZUCCA_H
+#define ZUCCA_H
+#include <cstdlib>
+#include <ctime>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+class Zucca
+{
+private:
+  float x;
+  float y;
+  int spostamento;
+  int cont;
+  bool vita;
+  bool changeBitmap;
+  bool andando_destra;
+  bool andando_sinistra;
+  ALLEGRO_BITMAP *dx1;
+  ALLEGRO_BITMAP *dx2;
+
+  ALLEGRO_BITMAP *sx1;
+  ALLEGRO_BITMAP *sx2;
+
+
+
+public:
+  Zucca();
+  Zucca(float ,float );
+  ~Zucca();
+
+  void drawZucca();
+  void muoviZucca(float,float);
+  bool getVita();
+  void setVita(bool );
+
+
+};
+#endif
